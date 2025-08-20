bbf7af04c73a7512180a74c8bb80cff59a5c43ba
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-08-20 14:54:21 +0530

    hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,22 @@ const Navbar = () => {
     const navTween = gsap.timeline({
       scrollTrigger: {
         trigger: 'nav',
-        start: "bottom top"
+        start: 'bottom top'
       }
-    })
+    });
 
-    navTween.fromTo('nav', { backgroundColor: "transparent" }, {
-      backgroundColor: "#000000050", backgroundFilter: 'blur(10px)', duration: 1, ease: 'power1.inOut'
-    })
+    navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+      backgroundColor: '#00000050',
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+    });
+
+    
   })
+
+
+
   return (
     <nav>
       <div>
