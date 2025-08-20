93c72c9da54bb4494de8608d9420a60c85f2b3cf
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-08-21 00:11:48 +0530

    footer

--- constant/index.js
+++ constant/index.js
@@ -120,17 +120,17 @@ const socials = [
     {
         name: "Instagram",
         icon: "/images/insta.png",
-        url: "#",
+        url: "https://www.instagram.com/_sameer_suryawanshi_/",
     },
     {
         name: "X (Twitter)",
         icon: "/images/x.png",
-        url: "#",
+        url: "https://www.instagram.com/_sameer_suryawanshi_/",
     },
     {
         name: "Facebook",
         icon: "/images/fb.png",
-        url: "#",
+        url: "https://www.facebook.com/sameer.suryawanshi1",
     },
 ];
 
